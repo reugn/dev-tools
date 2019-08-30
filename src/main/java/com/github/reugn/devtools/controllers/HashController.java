3e75b6a53440ec2f4ee9126484b550926222dff0
reugn <reugpro@gmail.com>
2019-08-31 00:27:06 +0300

    Implement Hash screen (close #2)

--- src/main/java/com/github/reugn/devtools/controllers/HashController.java
+++ src/main/java/com/github/reugn/devtools/controllers/HashController.java
@@ -1,15 +1,87 @@
 package com.github.reugn.devtools.controllers;
 
+import com.github.reugn.devtools.services.HashService;
 import com.github.reugn.devtools.utils.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HashController implements Initializable, Logger {
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    private TextArea hashInput;
 
+    @FXML
+    private TextArea hashOutput;
+
+    @FXML
+    private ComboBox hashAlgoSelector;
+
+    @FXML
+    private Button hashCalculateButton;
+
+    @FXML
+    private Button hashClearButton;
+
+    @FXML
+    private Label hashMessage;
+
+    @FXML
+    private void handleClear(final ActionEvent event) {
+        hashInput.setText("");
+        hashOutput.setText("");
+        hashMessage.setText("");
+    }
+
+    @FXML
+    private void handleCalculate(final ActionEvent event) {
+        try {
+            String enc;
+            String data = hashInput.getText();
+            switch (hashAlgoSelector.getSelectionModel().getSelectedItem().toString()) {
+                case "md5":
+                    enc = HashService.calculateHash(data, "MD5");
+                    break;
+                case "sha256":
+                    enc = HashService.calculateHash(data, "SHA-256");
+                    break;
+                case "Url encode":
+                    enc = HashService.urlEncode(data);
+                    break;
+                case "Url decode":
+                    enc = HashService.urlDecode(data);
+                    break;
+                case "Base64 encode":
+                    enc = HashService.base64Encode(data);
+                    break;
+                case "Base64 decode":
+                    enc = HashService.base64Decode(data);
+                    break;
+                default:
+                    enc = "NA";
+                    break;
+            }
+            hashOutput.setText(enc);
+        } catch (Exception e) {
+            hashMessage.setText(e.getMessage());
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void initialize(URL location, ResourceBundle resources) {
+        hashAlgoSelector.getItems().setAll("md5", "sha256", "Url encode", "Url decode",
+                "Base64 encode", "Base64 decode");
+        hashMessage.setPadding(new Insets(5));
+        hashMessage.setTextFill(Color.RED);
     }
 }
---
+++ src/main/java/com/github/reugn/devtools/services/HashService.java
@@ -0,0 +1,39 @@
+package com.github.reugn.devtools.services;
+
+import com.google.common.hash.HashCode;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class HashService {
+
+    private HashService() {
+    }
+
+    public static String calculateHash(String data, String algo) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(algo);
+        byte[] enc = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+        return HashCode.fromBytes(enc).toString();
+    }
+
+    public static String urlEncode(String value) throws UnsupportedEncodingException {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8.toString()).replace("+", "%20");
+    }
+
+    public static String urlDecode(String value) throws UnsupportedEncodingException {
+        return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+    }
+
+    public static String base64Encode(String value) throws UnsupportedEncodingException {
+        return Base64.getEncoder().encodeToString(value.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String base64Decode(String value) throws UnsupportedEncodingException {
+        return new String(Base64.getDecoder().decode(value.getBytes(StandardCharsets.UTF_8)));
+    }
+}
