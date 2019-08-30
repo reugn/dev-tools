ee3817ce64ebf337c632033fe21cfad68e78a2e0
reugn <reugpro@gmail.com>
2019-08-30 22:14:43 +0300

    Implement UUID screen (close #1)

--- src/main/java/com/github/reugn/devtools/controllers/UUIDController.java
+++ src/main/java/com/github/reugn/devtools/controllers/UUIDController.java
@@ -4,15 +4,34 @@ import com.github.reugn.devtools.utils.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 public class UUIDController implements Initializable, Logger {
 
     @FXML
+    private CheckBox uuidUpperCase;
+
+    @FXML
+    private CheckBox uuidHyphens;
+
+    @FXML
+    private TextField uuidResult;
+
+    @FXML
     private void handleGenerateAction(final ActionEvent event) {
-        debug("handleGenerateAction");
+        String uuid = UUID.randomUUID().toString();
+        if (uuidUpperCase.isSelected()) {
+            uuid = uuid.toUpperCase();
+        }
+        if (!uuidHyphens.isSelected()) {
+            uuid = uuid.replace("-", "");
+        }
+        uuidResult.setText(uuid);
     }
 
     @Override
