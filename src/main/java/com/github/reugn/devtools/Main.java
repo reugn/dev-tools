fa45cec0f827cbff539f752e7680c99b84586ac7
reugn <reugpro@gmail.com>
2019-08-30 22:16:26 +0300

    add stage icon

--- src/main/java/com/github/reugn/devtools/Main.java
+++ src/main/java/com/github/reugn/devtools/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
         primaryStage.setTitle("Dev-tools");
+        primaryStage.getIcons().add(new Image("/images/icons8-toolbox-64.png"));
         Scene scene = new Scene(root, 600, 400);
         scene.getStylesheets().add("/css/main.css");
         primaryStage.setScene(scene);
