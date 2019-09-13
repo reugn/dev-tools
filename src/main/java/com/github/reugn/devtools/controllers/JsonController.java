1d7cf7d8d1c28a98be9615b09aa578fffc2b826b
reugn <reugpro@gmail.com>
2019-09-13 22:34:58 +0300

    justify json screen buttons

--- src/main/java/com/github/reugn/devtools/controllers/JsonController.java
+++ src/main/java/com/github/reugn/devtools/controllers/JsonController.java
@@ -5,14 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class JsonController implements Initializable {
+
+    public Button clearSpacesButton;
+    public Button formatButton;
+    public Button clearButton;
 
     @FXML
     private Label jsonMessage;
@@ -55,5 +61,10 @@ public class JsonController implements Initializable {
         jsonArea.setPrefRowCount(1024);
         jsonMessage.setPadding(new Insets(5));
         jsonMessage.setTextFill(Color.RED);
+
+        HBox.setMargin(clearSpacesButton, new Insets(0, 5, 10, 0));
+        HBox.setMargin(formatButton, new Insets(0, 5, 10, 0));
+        HBox.setMargin(clearButton, new Insets(0, 5, 10, 0));
+        HBox.setMargin(jsonMessage, new Insets(0, 5, 10, 0));
     }
 }
