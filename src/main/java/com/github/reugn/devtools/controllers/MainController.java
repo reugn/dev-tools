10b9a6f9da51eb2b160e1bbd0cd5d5b58818c5f2
reugn <reugpro@gmail.com>
2019-09-15 00:41:03 +0300

    add About dialog

--- src/main/java/com/github/reugn/devtools/controllers/MainController.java
+++ src/main/java/com/github/reugn/devtools/controllers/MainController.java
@@ -5,12 +5,15 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
+import java.io.FileReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,6 +24,8 @@ public class MainController implements Initializable, Logger {
 
     @FXML
     private TabPane tabPane;
+
+    private Alert about;
 
     @FXML
     private void handleKeyInput(final InputEvent event) {
@@ -39,11 +44,28 @@ public class MainController implements Initializable, Logger {
 
     @FXML
     private void handleAboutAction(final ActionEvent event) {
-        debug("handleAboutAction");
+        about.show();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        about = new Alert(Alert.AlertType.INFORMATION);
+        about.setTitle("About");
+        about.setHeaderText("Development tools");
+        VBox vbox = new VBox();
+        Label l = new Label(" version: " + getVersion());
+        Hyperlink link = new Hyperlink("https://github.com/reugn/dev-tools");
+        vbox.getChildren().addAll(l, link);
+        about.getDialogPane().setContent(vbox);
+    }
 
+    private String getVersion() {
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader("pom.xml"));
+            return model.getVersion();
+        } catch (Exception e) {
+            return "NA";
+        }
     }
 }
