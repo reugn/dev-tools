8eec77cc36047433da82cf05b0a40352e653d9bd
reugn <reugpro@gmail.com>
2019-09-23 01:24:03 +0300

    implement password generator

---
+++ src/main/java/com/github/reugn/devtools/controllers/GeneratorController.java
@@ -0,0 +1,108 @@
+package com.github.reugn.devtools.controllers;
+
+import com.github.reugn.devtools.utils.Logger;
+import com.github.reugn.devtools.utils.PasswordGenerator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.security.InvalidParameterException;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+public class GeneratorController implements Initializable, Logger {
+
+    @FXML
+    private ComboBox<Integer> uuidAmount;
+
+    @FXML
+    private Label uuidAmountLabel;
+
+    @FXML
+    private CheckBox uuidUpperCase;
+
+    @FXML
+    private CheckBox uuidHyphens;
+
+    @FXML
+    private TextArea generatorResult;
+
+    @FXML
+    private CheckBox pwdLowChars;
+
+    @FXML
+    private CheckBox pwdDigits;
+
+    @FXML
+    private CheckBox pwdUpperChars;
+
+    @FXML
+    private CheckBox pwdSymbols;
+
+    @FXML
+    private TextField pwdLength;
+
+    @FXML
+    private Label pwdLengthLabel;
+
+    @FXML
+    private void handleGenerateUUIDAction(final ActionEvent event) {
+        StringBuilder buff = new StringBuilder();
+        Integer amount = uuidAmount.getValue();
+        for (int i = 0; i < amount; i++) {
+            String uuid = UUID.randomUUID().toString();
+            if (uuidUpperCase.isSelected()) {
+                uuid = uuid.toUpperCase();
+            }
+            if (!uuidHyphens.isSelected()) {
+                uuid = uuid.replace("-", "");
+            }
+            buff.append(uuid).append("\n");
+        }
+        generatorResult.setText(buff.toString());
+    }
+
+    @FXML
+    private void handleGeneratePasswordAction(final ActionEvent actionEvent) {
+        int length;
+        try {
+            pwdLength.setBorder(Border.EMPTY);
+            length = validatePasswordLength();
+        } catch (Exception e) {
+            pwdLength.setBorder(new Border(new BorderStroke(Color.RED,
+                    BorderStrokeStyle.SOLID, new CornerRadii(3), BorderWidths.DEFAULT)));
+            return;
+        }
+        PasswordGenerator generator = new PasswordGenerator.PasswordGeneratorBuilder()
+                .withLowerChars(pwdLowChars.isSelected())
+                .withDigits(pwdDigits.isSelected())
+                .withUpperChars(pwdUpperChars.isSelected())
+                .withSymbols(pwdSymbols.isSelected())
+                .build();
+        generatorResult.setText(generator.generate(length));
+    }
+
+    private int validatePasswordLength() throws Exception {
+        int length = Integer.parseInt(pwdLength.getText());
+        if (length < 1) throw new InvalidParameterException("Invalid password length");
+        return length;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        uuidAmount.getItems().setAll(1, 2, 3, 5, 10, 20, 50, 100);
+        uuidAmount.setValue(1);
+        uuidAmountLabel.setPadding(new Insets(5));
+        pwdLengthLabel.setPadding(new Insets(5));
+        pwdLength.setPrefWidth(64);
+        pwdLength.setText("16");
+
+        pwdLowChars.setSelected(true);
+        pwdDigits.setSelected(true);
+    }
+}
---
+++ src/main/java/com/github/reugn/devtools/utils/PasswordGenerator.java
@@ -0,0 +1,76 @@
+package com.github.reugn.devtools.utils;
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    private String dictionary;
+
+    private static String lowerCharsString = "abcdefghijklmnopqrstuvwxyz";
+    private static String digitsString = "0123456789";
+    private static String upperCharsString = lowerCharsString.toUpperCase();
+    private static String symbolsString = "!@#$%^&*?";
+
+    private PasswordGenerator(PasswordGeneratorBuilder builder) {
+        StringBuilder buff = new StringBuilder();
+        if (builder.useLower) {
+            buff.append(lowerCharsString);
+        }
+        if (builder.useDigits) {
+            buff.append(digitsString);
+        }
+        if (builder.useUpper) {
+            buff.append(upperCharsString);
+        }
+        if (builder.useSymbols) {
+            buff.append(symbolsString);
+        }
+        dictionary = buff.toString();
+    }
+
+    public String generate(int length) {
+        StringBuilder password = new StringBuilder(length);
+        Random random = new Random(System.nanoTime());
+        for (int i = 0; i < length; i++) {
+            password.append(dictionary.charAt(random.nextInt(dictionary.length())));
+        }
+        return password.toString();
+    }
+
+    public static class PasswordGeneratorBuilder {
+        private boolean useLower;
+        private boolean useUpper;
+        private boolean useDigits;
+        private boolean useSymbols;
+
+        public PasswordGeneratorBuilder() {
+            this.useLower = false;
+            this.useUpper = false;
+            this.useDigits = false;
+            this.useSymbols = false;
+        }
+
+        public PasswordGeneratorBuilder withLowerChars(boolean useLower) {
+            this.useLower = useLower;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder withUpperChars(boolean useUpper) {
+            this.useUpper = useUpper;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder withDigits(boolean useDigits) {
+            this.useDigits = useDigits;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder withSymbols(boolean usePunctuation) {
+            this.useSymbols = usePunctuation;
+            return this;
+        }
+
+        public PasswordGenerator build() {
+            return new PasswordGenerator(this);
+        }
+    }
+}
