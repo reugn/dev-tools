eaf0ac3dfc769d90384b89880371949b16062b25
reugn <reugpro@gmail.com>
2019-09-25 22:27:16 +0300

    wrap Application class for assembly build

--- src/main/java/com/github/reugn/devtools/Main.java
+++ src/main/java/com/github/reugn/devtools/Main.java
@@ -7,20 +7,27 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
-        primaryStage.setTitle("Dev-tools");
-        primaryStage.getIcons().add(new Image("/images/icons8-toolbox-64.png"));
-        Scene scene = new Scene(root, 800, 500);
-        scene.getStylesheets().addAll("/css/main.css", "/css/json-highlighting.css");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        App.main(args);
+    }
+
+    public static class App extends Application {
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+            primaryStage.setTitle("Dev-tools");
+            primaryStage.getIcons().add(new Image("/images/icons8-toolbox-64.png"));
+            Scene scene = new Scene(root, 800, 500);
+            scene.getStylesheets().addAll("/css/main.css", "/css/json-highlighting.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
 }
