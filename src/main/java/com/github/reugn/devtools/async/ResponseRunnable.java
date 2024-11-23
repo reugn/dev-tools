bd24b4885deb1e7dc39de50cfba41dc0f3a03a18
reugn <reugpro@gmail.com>
2024-11-23 12:05:56 +0200

    remove renamed files

--- src/main/java/com/github/reugn/devtools/async/ResponseRunnable.java
+++
@@ -1,9 +0,0 @@
-package com.github.reugn.devtools.async;
-
-import com.github.reugn.devtools.models.RestResponse;
-
-@FunctionalInterface
-public interface ResponseRunnable {
-
-    void run(RestResponse response);
-}
